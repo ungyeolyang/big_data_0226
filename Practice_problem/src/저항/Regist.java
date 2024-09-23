dd129b0723081b45a7e1a0c489073c078ffeacca
gameyang <yuy8150@gmailc.com>
2024-09-23 16:59:28 +0900

    09.23 코딩테스트 백준 1076번(브론즈 2)

--- Practice_problem/src/저항/Regist.java
+++ Practice_problem/src/저항/Regist.java
@@ -2,50 +2,32 @@ package 저항;
 
 import java.util.Scanner;
 
-public class Regist {
+public class Regist{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[][] regist = {{"black", "0", "1"}, {"brown", "1", "10"}, {"red", "2", "100"},
-                {"orange", "3", "1,000"}, {"yellow", "4", "10,000"}, {"green", "5", "100,000"},
-                {"blue", "6", "1,000,000"}, {"violet", "7", "10,000,000"}, {"grey", "8", "100,000,000"},
-                {"white", "9", "1,000,000,000"}};
-        String a = sc.next();
-        String b = sc.next();
-        String c = sc.next();
-
-        for (int i = 0; i < regist.length; i++) {
-            if (regist[i][0].contains(a)) {
-                a = regist[i][1];
-            }
-            if (regist[i][0].contains(b)) {
-                b = regist[i][1];
-            }
-            if (regist[i][0].contains(c)) {
-                c = regist[i][2].substring(1);
-            }
+        String[] color = {"black","brown","red","orange","yellow","green","blue","violet","grey","white"};
+        String x = sc.nextLine();
+        String y = sc.nextLine();
+        String z = sc.nextLine();
+        String result =null;
+        for(int i = 0; i <color.length; i++){
+            if(color[i].equals(x)){
+                result = String.valueOf(i);
+                break;}
         }
-        System.out.println(a + b + c);
+        for(int j = 0; j <color.length; j++){
+            if(color[j].equals(y)){
+                if(result.equals("0")){result=String.valueOf(j);}
+                else{result+=String.valueOf(j);}
+                break;}
+        }
+        for(int k = 0; k <color.length; k++){
+            if(color[k].equals(z)){
+                if(result.equals("0")){result="0";}
+                else{result += "0".repeat(k);}
+                break;}
+        }
+        System.out.println(result);
     }
 }
 
-//        String[] regists = {"black", "brown", "red", "orange", "yellow", "green",
-//                "blue", "violet", "grey", "white"};
-//        String first = sc.next();
-//        String second = sc.next();
-//        String third = sc.next();
-//        int findex = 0, sindex =0, tindex = 0;
-//
-//        for(int i = 0; i < regist.length; i++){
-//            if(regists[i].equals(first)){
-//                findex = i;
-//            }
-//            if(regists[i].equals(second)){
-//                sindex = i;
-//            }
-//            if(regists[i].equals(third)){
-//                tindex = i;
-//            }
-//        }
-//        System.out.println(((findex*10)+sindex)* Math.pow(10,tindex));
-//    }
-//} // math.pow() 사용하자
